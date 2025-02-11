00f6f9360e96ba28e5372a7445fdb374e547904d
Bishoeri000 <Bishoeri000@warren.k12.in.us>
2025-02-11 10:15:45 -0500

    Second Commit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,33 @@
-#include <iostream>
+#include "raylib.h"
 
+int main() {
+    InitWindow(800, 600, "Raylib Game Loop Example");
+    SetTargetFPS(60);
 
+    while (!WindowShouldClose()) {
+        const float timeStep = 1.0f / 60.0f;
+        float accumulator = 0.0f;
+        float speedX = 2.0f;
+        float posX = 30.f;
 
+        while (!WindowShouldClose()) {
+            float deltaTime = GetFrameTime();
+            accumulator += deltaTime;
 
+        while (accumulator >= timeStep) {
+            posX += speedX;
+            accumulator -= timeStep;
+        }
 
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawCircle((int) posX, 200 + 300, BROWN);
+            EndDrawing();
+    }
 
-
-
-
-
-
+    CloseWindow();
+    return 0;
+}
 
 // TIP See CLion help at <a
 // href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
